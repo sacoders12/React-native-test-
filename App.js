066b79254ee5e5e4a644479ee1756f32fd9c9726
lolo <hermawan.agung12@gmail.com>
2021-02-11 10:17:29 +0700

    merubah informasi default dari welcome RN

--- App.js
+++ App.js
@@ -42,25 +42,6 @@ const App: () => React$Node = () => {
             <View style={styles.sectionContainer}>
               <Text>Hello World..</Text>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Hello World</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
           </View>
         </ScrollView>
       </SafeAreaView>
